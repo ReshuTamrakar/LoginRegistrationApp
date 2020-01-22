2019402012ba824737087ef076b512827c994e64
reshu.tamrakar@gmail.com <reshu.tamrakar@gmail.com>
2020-01-22 19:13:50 +0530

    Convert html to pdf Added pdfController

---
+++ LoginRegistrationApp/Controllers/PdfController.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+
+using iTextSharp.text;
+using iTextSharp.text.html.simpleparser;
+using iTextSharp.text.pdf;
+using System.Configuration;
+
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+
+
+namespace LoginRegistrationApp.Controllers
+{
+    public class PdfController : Controller
+    {
+        // GET: Pdf
+
+        public void DownloadPDF()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("<header class='clearfix'>");
+            sb.Append("<h1>INVOICE</h1>");
+            sb.Append("<div id='company' class='clearfix'>");
+            sb.Append("<div>Company Name</div>");
+            sb.Append("<div>455 John Tower,<br /> AZ 85004, US</div>");
+            sb.Append("<div>(602) 519-0450</div>");
+            sb.Append("<div><a href='mailto:company@example.com'>company@example.com</a></div>");
+            sb.Append("</div>");
+            sb.Append("<div id='project'>");
+            sb.Append("<div><span>PROJECT</span> Website development</div>");
+            sb.Append("<div><span>CLIENT</span> John Doe</div>");
+            sb.Append("<div><span>ADDRESS</span> 796 Silver Harbour, TX 79273, US</div>");
+            sb.Append("<div><span>EMAIL</span> <a href='mailto:john@example.com'>john@example.com</a></div>");
+            sb.Append("<div><span>DATE</span> April 13, 2016</div>");
+            sb.Append("<div><span>DUE DATE</span> May 13, 2016</div>");
+            sb.Append("</div>");
+            sb.Append("</header>");
+            sb.Append("<main>");
+            sb.Append("<table>");
+            sb.Append("<thead>");
+            sb.Append("<tr>");
+            sb.Append("<th class='service'>SERVICE</th>");
+            sb.Append("<th class='desc'>DESCRIPTION</th>");
+            sb.Append("<th>PRICE</th>");
+            sb.Append("<th>QTY</th>");
+            sb.Append("<th>TOTAL</th>");
+            sb.Append("</tr>");
+            sb.Append("</thead>");
+            sb.Append("<tbody>");
+            sb.Append("<tr>");
+            sb.Append("<td class='service'>Design</td>");
+            sb.Append("<td class='desc'>Creating a recognizable design solution based on the company's existing visual identity</td>");
+            sb.Append("<td class='unit'>$400.00</td>");
+            sb.Append("<td class='qty'>2</td>");
+            sb.Append("<td class='total'>$800.00</td>");
+            sb.Append("</tr>");
+            sb.Append("<tr>");
+            sb.Append("<td colspan='4'>SUBTOTAL</td>");
+            sb.Append("<td class='total'>$800.00</td>");
+            sb.Append("</tr>");
+            sb.Append("<tr>");
+            sb.Append("<td colspan='4'>TAX 25%</td>");
+            sb.Append("<td class='total'>$200.00</td>");
+            sb.Append("</tr>");
+            sb.Append("<tr>");
+            sb.Append("<td colspan='4' class='grand total'>GRAND TOTAL</td>");
+            sb.Append("<td class='grand total'>$1,000.00</td>");
+            sb.Append("</tr>");
+            sb.Append("</tbody>");
+            sb.Append("</table>");
+            sb.Append("<div id='notices'>");
+            sb.Append("<div>NOTICE:</div>");
+            sb.Append("<div class='notice'>A finance charge of 1.5% will be made on unpaid balances after 30 days.</div>");
+            sb.Append("</div>");
+            sb.Append("</main>");
+            sb.Append("<footer>");
+            sb.Append("Invoice was created on a computer and is valid without the signature and seal.");
+            sb.Append("</footer>");
+
+
+            //string HTMLContent = sb;
+            Response.Clear();
+            Response.ContentType = "application/pdf";
+            Response.AddHeader("content-disposition", "attachment;filename=" + "PDFfile.pdf");
+            Response.Cache.SetCacheability(HttpCacheability.NoCache);
+            Response.BinaryWrite(GetPDF(sb.ToString()));
+            Response.End();
+        }
+
+        public byte[] GetPDF(string pHTML)
+        {
+            byte[] bPDF = null;
+
+            MemoryStream ms = new MemoryStream();
+            TextReader txtReader = new StringReader(pHTML);
+
+            // 1: create object of a itextsharp document class  
+            Document doc = new Document(PageSize.A4, 25, 25, 25, 25);
+
+            // 2: we create a itextsharp pdfwriter that listens to the document and directs a XML-stream to a file  
+            PdfWriter oPdfWriter = PdfWriter.GetInstance(doc, ms);
+
+            // 3: we create a worker parse the document  
+            HTMLWorker htmlWorker = new HTMLWorker(doc);
+
+            // 4: we open document and start the worker on the document  
+            doc.Open();
+            htmlWorker.StartDocument();
+
+
+            // 5: parse the html into the document  
+            htmlWorker.Parse(txtReader);
+
+            // 6: close the document and the worker  
+            htmlWorker.EndDocument();
+            htmlWorker.Close();
+            doc.Close();
+
+            bPDF = ms.ToArray();
+
+            return bPDF;
+        }
+    }
+}
